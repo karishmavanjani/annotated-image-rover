f5db283c3aa9f2a404374cd4cb3b0ae954f4d66c
Karishma Vanjani <karishmavanjani@Karishmas-MacBook-Pro.local>
2019-02-19 20:07:39 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$("body").on("click", ".dot ", function (e) {
+	e.preventDefault();
+	const $this_dot = $(this);
+
+	$this_dot.addClass("active");
+});
+
+$("body").on("click", ".dot .close", function (e) {
+	e.preventDefault();
+	e.stopPropagation();
+	const $this_close = $(this);
+	const $this_dot = $this_close.closest(".dot");
+
+	$this_dot.removeClass("active");
+});
